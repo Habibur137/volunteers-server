e9ca87dea0b1488775f808e2511266bead962113
Habibur137 <infernohabib@gmail.com>
2022-04-27 23:38:51 +0600

    create jwt token

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+var jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+//middleware
+const varefytoken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unathorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send("Forbiden");
+    }
+    req.decoded = decoded;
+  });
+
+  next();
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vjag1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -23,6 +40,15 @@ const run = async () => {
     const bookingCollection = client
       .db("volunteersServices")
       .collection("booking");
+
+    // auth json web token generate for login user ------=======-------=======--------
+    app.post("/login", (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
 
     // get all data -----======-------=======-------=======-------======-------
     app.get("/volunteers", async (req, res) => {
@@ -58,12 +84,19 @@ const run = async () => {
       res.send(result);
     });
     // get booking data--------=======----------==========------------=============--------
-    app.get("/volunteersbooking", async (req, res) => {
+    app.get("/volunteersbooking", varefytoken, async (req, res) => {
+      // const authHeader = req.headers.authorization;
+      // console.log(authHeader);
+      const decodedEmail = req.decoded.email;
       const email = req.query.email;
-      const query = { email };
-      const cursor = bookingCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
+      if (decodedEmail === email) {
+        const query = { email };
+        const cursor = bookingCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      } else {
+        res.status(403).send("forbidden access");
+      }
     });
   } finally {
   }
